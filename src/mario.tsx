c952ccd72763d7e2abcd89ed8161e300c39d11a6
Florian Rappl <florian.rappl@smapiot.com>
2019-11-18 21:51:35 +0100

    Lazy load mario page

--- src/mario.tsx
+++ src/mario.tsx
@@ -1,16 +1,17 @@
 import './Styles/mario.scss';
+import * as React from 'react';
 import { Level } from './engine';
 import { assets } from './assets';
 import { keys } from './keys';
 import { LevelFormat } from './types';
 
-export interface MarioGameOptions {
+interface MarioGameOptions {
   sound?: boolean;
   level?: number;
   levels?: Array<LevelFormat>;
 }
 
-export function appendMarioTo(host: Element, options: MarioGameOptions = {}) {
+function appendMarioTo(host: Element, options: MarioGameOptions = {}) {
   const loadingLevels = options.levels || import('./levels').then(m => m.default);
 
   return Promise.resolve(loadingLevels).then(levels => {
@@ -27,3 +28,25 @@ export function appendMarioTo(host: Element, options: MarioGameOptions = {}) {
     return level;
   });
 }
+
+let gamePromise: Promise<Level>;
+let container: HTMLDivElement;
+
+const MarioGame: React.FC = () => {
+  const host = React.useRef<HTMLDivElement>();
+  React.useEffect(() => {
+    if (!gamePromise) {
+      container = document.createElement('div');
+      gamePromise = appendMarioTo(container, {
+        sound: true,
+      });
+    }
+
+    host.current.appendChild(container);
+    gamePromise.then(game => game.start());
+    return () => gamePromise.then(game => game.pause());
+  });
+  return <div ref={host} />;
+};
+
+export default MarioGame;
